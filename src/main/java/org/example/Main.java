3fd5a45d349e5b6c6620ac542b52893c086d64b6
Radoslav <manev.radoslav@gmail.com>
2023-06-22 22:04:06 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+    static Logger logger = Logger.getLogger(String.valueOf(Main.class));
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        logger.info("Info");
+        logger.debug("Debug");
+    }
+}
